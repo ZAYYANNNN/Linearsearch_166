d887664700b73aa9f2fd5b131c3b3ac2ea2c17f9
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-04-14 11:05:51 +0700

    meyimpan angka array yang akan di masukan

--- Linearsearch_166.cpp
+++ Linearsearch_166.cpp
@@ -22,10 +22,10 @@ void input()
 	cout << "\n----------------------\n";
 	cout << "Enter Array ELements \n";
 	cout << "----------------------\n";
-	for (i = 0;) i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << "> ";
-		cin >> arr[i]
+		cin >> arr[i];
 	}
 
 }
